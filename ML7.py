31556431879eaa8c495394d987ae696f454ad629
MukundaHari R <146832875+mukihari@users.noreply.github.com>
2025-07-15 12:49:17 +0530

    Update ML7.py

--- ML7.py
+++ ML7.py
@@ -1,7 +1,9 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression from sklearn.datasets import make_regression
+from sklearn.linear_model import LinearRegression 
+from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score 
+import matplotlib.pyplot as plt
 # Generate a synthetic dataset
 X,y	=	make_regression(n_samples=1000,	n_features=1,	noise=20, random_state=42)
 # Split the dataset into training and testing sets 
@@ -19,6 +21,9 @@ r2 = r2_score(y_test, predictions)
 print("Mean Squared Error (MSE):\n",mse) 
 print("R-squared:\n",r2)
 # Plotting the regression line (optional) 
-plt.scatter(X_test, y_test, color='blue') plt.plot(X_test, predictions, color='red', linewidth=3) 
+plt.scatter(X_test, y_test, color='blue') 
+plt.plot(X_test, predictions, color='red', linewidth=3) 
 plt.xlabel('X')
-plt.ylabel('y') plt.title('Linear Regression') plt.show()
+plt.ylabel('y') 
+plt.title('Linear Regression') 
+plt.show()
