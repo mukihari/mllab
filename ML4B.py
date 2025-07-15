319556708c373f2d4b09d7a8e2f6895903951cba
MukundaHari R <146832875+mukihari@users.noreply.github.com>
2025-07-15 12:41:28 +0530

    Update ML4B.py

--- ML4B.py
+++ ML4B.py
@@ -1,6 +1,7 @@
 import pandas as pd # Load Excel file
 excel_data = pd.read_excel('Sample - Superstore.xlsx', sheet_name='Orders') # Display the first few rows of the Excel file
-print("First few rows of Excel file:") print(excel_data.head())
+print("First few rows of Excel file:") 
+print(excel_data.head())
 # Summary statistics
 print("\nSummary statistics of Excel file:") 
 print(excel_data.describe())
