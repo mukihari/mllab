7c88cb46e2a24ede1028b4ce5752b4c283877874
MukundaHari R <146832875+mukihari@users.noreply.github.com>
2025-07-15 12:13:54 +0530

    ML4B.py
    Write a program to Load and explore the dataset of .CVS and excel files using pandas.

---
+++ ML4B.py
@@ -0,0 +1,9 @@
+import pandas as pd # Load Excel file
+excel_data = pd.read_excel('Sample - Superstore.xlsx', sheet_name='Orders') # Display the first few rows of the Excel file
+print("First few rows of Excel file:") print(excel_data.head())
+# Summary statistics
+print("\nSummary statistics of Excel file:") 
+print(excel_data.describe())
+# Information about columns
+print("\nInformation about columns in Excel file:") 
+print(excel_data.info())
